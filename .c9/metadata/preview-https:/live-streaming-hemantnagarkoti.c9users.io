d9eebc232b18d2d2396379c6c8f03bd6d75aaca2
hemant <hemant.nagarkoti@armentum.co>
2017-11-01 12:16:49 +0530

    first commit

---
+++ .c9/metadata/preview-https:/live-streaming-hemantnagarkoti.c9users.io
@@ -0,0 +1,1 @@
+{"changed":false,"filter":false,"title":"[P] https://live-streaming-hemantnagarkoti.c9users.io","value":"","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://live-streaming-hemantnagarkoti.c9users.io","previewer":"preview.browser","disableInjection":true},"timestamp":1508825558739}
--- app.js
+++ app.js
@@ -13,13 +13,34 @@ app.use(express.static(__dirname + "/public"));
 app.get('/', function(req, res){
   res.redirect('index.html');
 })
-
+var fs = require('fs');
+// var wstream = fs.createWriteStream('videoBinary.txt');
+var wstream = fs.createWriteStream('videoBinary1.txt');
 io.on('connection', function(socket){
+  socket.emit('news', { hello: 'world' });
+  socket.on('stream', function( data ){
+    console.log('data:------- ', data)
+    console.log('time:-- ', (new Date()).toISOString(),'data recieved:- ', data);
+    // var buffArr = new ArrayBuffer(data);
+    // console.log('buffArr:---- ',buffArr);
+    wstream.write(data);
+    fs.writeFile("videoBinary1.bin", data,  "binary",function(err) { 
+      if(err){ console.log(err);} 
+      else {
+        //console.log('write done!')
+      }
+      });
+    //socket.broadcast.emit('stream', data)
 
-  socket.on('stream', function(image){
-    socket.broadcast.emit('stream', image)
+    socket.broadcast.emit('stream', data)
+  })
+  socket.on('end', function(data){
+    console.log('called');
+    res.end();
   })
 })
+
+
 
 http.listen(port, function(){
   log.info('Server started listening at port ', port)
